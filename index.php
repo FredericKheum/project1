952170b88cd61190fc3459d75355386c12e2d930
FredericKheum <frederic.kheum@efrei.net>
2016-03-10 19:54:33 +0100

    Added files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Medica</title>
+		<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/style.css" />
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    <header>
+		<h1>Bienvenue sur Medica</h1>
+		<nav class="navbar navbar-inverse">
+  ...
+</nav>
+	</header>
+    
+    
+    <!-- Le menu -->
+    
+   <div> 
+		
+		<form action="connexion.php" method="post">
+			<p>
+				<h2>Patient</h2>
+				<label for="carte_vitale">Numéro Carte Vital</label>
+				<input type = "text" name="carte_vitale" id="vitale"/>
+				<input type="submit" value="Connexion"/>
+				
+				<h2>Médecin</h2>
+				<label for="mon_id">Identifiant</label>
+				<input type = "text" name="" id="mon_id" />
+				<label for="pass">Mot de passe </label>
+				<input type="password" name="pass" id="pass"/>
+				<input type="submit" value="Connexion"/>
+			</p>		
+
+		</form>
+   <div>    
+    <!-- Le corps -->
+    
+   
+    
+    <!-- Le pied de page -->
+
+    
+    </body>
+</html>
---
+++ page1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Medica-page 1</title>
+    </head>
+	
+	<p>Bonjour <?php echo $_GET['prenom'].' '. $_GET['nom']; ?> !</p>
+</html>
