067d127008294b644814f84a725be0cd961c8694
FredericKheum <frederic.kheum@efrei.net>
2016-03-12 11:55:28 +0100

    modularisation des fichiers

--- page1.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Medica-page 1</title>
-    </head>
-	
-	<p>Bonjour <?php echo $_GET['prenom'].' '. $_GET['nom']; ?> !</p>
-</html>
